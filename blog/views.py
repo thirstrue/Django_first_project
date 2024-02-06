213fd5c09096395f3684668a47b02e6a0a6fb85b
thirstrue <mybalkanmix@gmail.com>
2024-02-06 19:19:47 +0500

    Django Proj v1.1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+def index(request):
+    categories = Category.objects.all()
+    articles = Article.objects.all()
+
+    context = {
+        'title': 'Главная страница',
+        'articles': articles,
+        'categories': categories
+    }
+
+    return render(request, 'index.html', context)
